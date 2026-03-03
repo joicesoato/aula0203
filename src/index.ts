a5e3d0a7c885b3ec590f714f5dc511dd4f47fb80
Joice Soato <soatojoyce@gmail.com>
2026-03-03 01:31:37 +0000

    aula 01 API - backend

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+function main(): void {
+    console.log("Ola Typescript!!!");
+}
+
+main();
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req : Request, res : Response)=>{
+    res.send("Ola express!!!");
+});
+
+app.get("/saudacao/:nome", (req, res) => {
+    const nome = req.params.nome;
+    res.send("Ola " + nome);
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor executando em localhost");
+});
+
+// const - let - var 
+// c - criar uma variavel que não ira mudar o valor durante a criação da aplicação (imutavel)
+// l - cria variavel que muda e é restrito apenas ao bloco 
+// v - 'publico' acessivel todo o codigo
+// get - solicitar algum recurso
+
+
+//  function main() : void { 
+// 
+// } - função nomeada 
+// () => {}
